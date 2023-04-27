904201b2e6b25ae5c23afe6ac50bd06c2a233de4
T14s <122837733+looonnng@users.noreply.github.com>
2023-04-26 20:51:10 -0500

    Create function game - not fully functional; Format with Prettier

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 // Create a function called getComputerChoice
 // that return Rock, Paper, Scissors randomly
-const options = ['Rock', 'Paper', 'Scissors'];
+const options = ["Rock", "Paper", "Scissors"];
 function getComputerChoice(options) {
-    return options[Math.floor(Math.random() * options.length)];
+  return options[Math.floor(Math.random() * options.length)];
 }
-
 
 // Create a function called playRound() that play a single round of RPS
 // takes two parameters - playerSelection and computer Selection
@@ -12,57 +11,76 @@ function getComputerChoice(options) {
 // make playerSelection parameter case-insensitive
 
 let computerSelection = getComputerChoice(options);
-let playerChoice = prompt('Player Choice: Rock, Paper, or Scissors?', '');
+let playerChoice = prompt("Player Choice: Rock, Paper, or Scissors?", "");
 
 function checkPlayerChoice(playerChoice) {
-    if (playerChoice.toLowerCase() == 'rock' || playerChoice.toLowerCase() == 'paper' || playerChoice.toLowerCase() == 'scissors') {
-        return playerChoice
-    }
-    const typoMessage = 'Please enter Rock, Paper, or Scissors!';
-    return typoMessage;
+  if (
+    playerChoice.toLowerCase() == "rock" ||
+    playerChoice.toLowerCase() == "paper" ||
+    playerChoice.toLowerCase() == "scissors"
+  ) {
+    return playerChoice;
+  }
+  const typoMessage = "Please enter Rock, Paper, or Scissors!";
+  return typoMessage;
 }
 
 /* 
 const playerSelection = playerChoice.charAt(0).toUpperCase() + 
-console.log(playerSelection) */
-
-
-
+return(playerSelection) */
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = checkPlayerChoice(playerChoice);
+  playerSelection = checkPlayerChoice(playerChoice);
 
-    if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
-        console.log('Tie') ;
-    } else if (playerSelection.toLowerCase() == 'rock') {
-        if (computerSelection == 'Paper') {
-            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-        } else {
-            console.log(`You Win! ${playerSelection} beats ${computerSelection}`)
-        }
-    } else if (playerSelection.toLowerCase() == 'paper') {
-        if (computerSelection == 'Scissors') {
-            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-        } else {
-            console.log(`You Win! ${playerSelection} beats ${computerSelection}`)
-        } 
-    } else if (playerSelection.toLowerCase() == 'scissors') {
-        if (computerSelection == 'rock') {
-            console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-        } else {
-            console.log(`You Win! ${playerSelection} beats ${computerSelection}`)
-        }
+  if (playerSelection.toLowerCase() == computerSelection.toLowerCase()) {
+    return "Tie";
+  } else if (playerSelection.toLowerCase() == "rock") {
+    if (computerSelection == "Paper") {
+      return `You Lose! ${computerSelection} beats ${playerSelection}`;
     } else {
-        console.log(playerSelection);
+      return `You Win! ${playerSelection} beats ${computerSelection}`;
     }
-
+  } else if (playerSelection.toLowerCase() == "paper") {
+    if (computerSelection == "Scissors") {
+      return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+      return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+  } else if (playerSelection.toLowerCase() == "scissors") {
+    if (computerSelection == "rock") {
+      return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+      return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+  } else {
+    return playerSelection;
+  }
 }
 
-playRound(playerChoice, computerSelection);
+//playRound(playerChoice, computerSelection);
 
 // Create a function called game()
 // Combine previous function to play a 5 round
 // that keeps score
 // reports a winner or loser at the end
 
-function game()
+function game() {
+  let playerScore = 0;
+  let copmuterScore = 0;
+
+  let checkWin = /You Win/;
+  let checkLose = /You Lose/;
+
+  let roundResult = playRound(playerChoice, computerSelection);
+  if (checkWin.test(roundResult)) {
+    playerScore += 1;
+    console.log(roundResult);
+  } else if (checkLose.test(roundResult)) {
+    copmuterScore += 1;
+    console.log(roundResult);
+  } else console.log("Tie");
+}
+
+
+game();
+// for (let gameRound = 1; gameRound < 6; gameRound++) {}
