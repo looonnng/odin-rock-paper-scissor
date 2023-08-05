e0dbda1c753015509af74396e939a11bb1602b0d
T14s <122837733+looonnng@users.noreply.github.com>
2023-08-05 13:17:39 -0500

    Rewrite game() to reset when first person get to five points

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ const buttons = document.querySelectorAll("button");
 
 const getPlayerChoice = (e) => e.target.textContent;
 
-buttons.forEach((button) => button.addEventListener("click", game));
+buttons.forEach((button) => button.addEventListener("click", game)); // Click one of the opiton to start game
 
 const resultDisplay = document.querySelector("#result");
+
 const buttonContainer = document.querySelector(".buttons");
 
 const scoreBoard = document.createElement("div");
+scoreBoard.classList.toggle("result");
 
 const container = document.querySelector(".container");
 
@@ -27,48 +29,66 @@ function playRound(e) {
   const computerSelection = getComputerChoice();
 
   if (playerSelection == computerSelection) {
-    resultDisplay.textContent = "Tie";
+    resultDisplay.textContent = `Round ${roundNum}: Tie`;
   } else if (playerSelection == "Rock") {
     if (computerSelection == "Paper") {
-      resultDisplay.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+      resultDisplay.textContent = `Round ${roundNum}: You lose! ${computerSelection} beats ${playerSelection}`;
     } else {
-      resultDisplay.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+      resultDisplay.textContent = `Round ${roundNum}: You win! ${playerSelection} beats ${computerSelection}`;
     }
   } else if (playerSelection == "Paper") {
     if (computerSelection == "Scissors") {
-      resultDisplay.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+      resultDisplay.textContent = `Round ${roundNum}: You lose! ${computerSelection} beats ${playerSelection}`;
     } else {
-      resultDisplay.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+      resultDisplay.textContent = `Round ${roundNum}: You win! ${playerSelection} beats ${computerSelection}`;
     }
   } else if (playerSelection == "Scissors") {
     if (computerSelection == "Rock") {
-      resultDisplay.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
+      resultDisplay.textContent = `Round ${roundNum}: You lose! ${computerSelection} beats ${playerSelection}`;
     } else {
-      resultDisplay.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
+      resultDisplay.textContent = `Round ${roundNum}: You win! ${playerSelection} beats ${computerSelection}`;
     }
   }
 }
 
-//Rewrite Game to track score and play first to 5
 
+let roundNum = 1;
 let playerScore = 0;
 let computerScore = 0;
 
-function game(e) {
+function trackScore(e) {
   playRound(e);
 
   if (resultDisplay.textContent.includes("You win")) {
+    roundNum += 1;
     playerScore += 1;
     scoreBoard.textContent = `${playerScore} : ${computerScore}`;
-    container.insertBefore(scoreBoard, resultDisplay);
+    container.insertBefore(scoreBoard, buttonContainer);
   } else if (resultDisplay.textContent.includes("You lose")) {
+    roundNum += 1;
     computerScore += 1;
     scoreBoard.textContent = `${playerScore} : ${computerScore}`;
-    container.insertBefore(scoreBoard, resultDisplay);
+    container.insertBefore(scoreBoard, buttonContainer);
   } else if (resultDisplay.textContent.includes("Tie")) {
+    roundNum += 1;
     scoreBoard.textContent = `${playerScore} : ${computerScore}`;
-    container.insertBefore(scoreBoard, resultDisplay);
+    container.insertBefore(scoreBoard, buttonContainer);
   }
 }
 
-scoreBoard.style.marginBottom = "1rem";
+function game(e) {
+  trackScore(e);
+
+  if (playerScore == 5) {
+    resultDisplay.textContent = "Congratulations! You won.";
+    playerScore = 0;
+    computerScore = 0;
+    roundNum = 1;
+  }
+  if (computerScore == 5) {
+    resultDisplay.textContent = "Game over! Machine won.";
+    playerScore = 0;
+    computerScore = 0;
+    roundNum = 1;
+  }
+}
