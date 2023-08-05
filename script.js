72a4b2a161ff934f6f9707279d9f3148aacfbbdc
T14s <122837733+looonnng@users.noreply.github.com>
2023-08-04 23:30:27 -0500

    Modified game() to keep track of scores

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 // DOM Manipulation and Event Captures
 const buttons = document.querySelectorAll("button");
 
-const getPlayerChoice = e => e.target.textContent;
+const getPlayerChoice = (e) => e.target.textContent;
 
-buttons.forEach(button => button.addEventListener("click", playRound));
+buttons.forEach((button) => button.addEventListener("click", game));
 
-const resultDisplay = document.querySelector('#result');
+const resultDisplay = document.querySelector("#result");
+const buttonContainer = document.querySelector(".buttons");
 
+const scoreBoard = document.createElement("div");
 
+const container = document.querySelector(".container");
 
 // Create a function called getComputerChoice
 // that return Rock, Paper, Scissors randomly
@@ -19,60 +22,53 @@ function getComputerChoice() {
 // Create a function called playRound() that play a single round of RPS
 // takes two parameters - playerSelection and computer Selection
 
-
-
-
 function playRound(e) {
-
   const playerSelection = getPlayerChoice(e);
   const computerSelection = getComputerChoice();
 
   if (playerSelection == computerSelection) {
-    resultDisplay.textContent = "Tie"; 
+    resultDisplay.textContent = "Tie";
   } else if (playerSelection == "Rock") {
     if (computerSelection == "Paper") {
-      resultDisplay.textContent = `You lose! ${computerSelection} beats ${playerSelection}` ;
+      resultDisplay.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
     } else {
-      resultDisplay.textContent = `You win! ${playerSelection} beats ${computerSelection}` ;
+      resultDisplay.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
     }
   } else if (playerSelection == "Paper") {
     if (computerSelection == "Scissors") {
-      resultDisplay.textContent = `You lose! ${computerSelection} beats ${playerSelection}` ;
+      resultDisplay.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
     } else {
-      resultDisplay.textContent = `You win! ${playerSelection} beats ${computerSelection}` ;
+      resultDisplay.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
     }
   } else if (playerSelection == "Scissors") {
     if (computerSelection == "Rock") {
-      resultDisplay.textContent = `You lose! ${computerSelection} beats ${playerSelection}` ;
+      resultDisplay.textContent = `You lose! ${computerSelection} beats ${playerSelection}`;
     } else {
-      resultDisplay.textContent = `You win! ${playerSelection} beats ${computerSelection}` ;
+      resultDisplay.textContent = `You win! ${playerSelection} beats ${computerSelection}`;
     }
   }
-} 
-
-
+}
 
 //Rewrite Game to track score and play first to 5
+
 let playerScore = 0;
 let computerScore = 0;
 
 function game(e) {
-  const playerSelection = getplayerChoice(e);
-  const computerSelection = getComputerChoice();
+  playRound(e);
 
-  let roundString = playRound(playerSelection, computerSelection);
-
-  if (roundString.includes("You Win")) {
+  if (resultDisplay.textContent.includes("You win")) {
     playerScore += 1;
-    console.log(roundString + ` ${playerScore} : ${computerScore}`);
-  } else if (roundString.includes("You Lose")) {
+    scoreBoard.textContent = `${playerScore} : ${computerScore}`;
+    container.insertBefore(scoreBoard, resultDisplay);
+  } else if (resultDisplay.textContent.includes("You lose")) {
     computerScore += 1;
-    console.log(roundString + ` ${playerScore} : ${computerScore}`);
-  } else if (roundString.includes("Tie")) {
-    console.log(roundString + ` ${playerScore} : ${computerScore}`);
-  } else {
-    console.log(roundString);
+    scoreBoard.textContent = `${playerScore} : ${computerScore}`;
+    container.insertBefore(scoreBoard, resultDisplay);
+  } else if (resultDisplay.textContent.includes("Tie")) {
+    scoreBoard.textContent = `${playerScore} : ${computerScore}`;
+    container.insertBefore(scoreBoard, resultDisplay);
   }
 }
 
-
+scoreBoard.style.marginBottom = "1rem";
